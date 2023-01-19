3b26087b39aeb6e5cef7c16f6e116e8b4fd049fe
chloeeliu <liuxinchi9@gmail.com>
2023-01-19 17:52:21 +0800

    Update regression and classification.py

--- regression and classification.py
+++ regression and classification.py
@@ -1,50 +1,163 @@
-# UNQ_C3
-# GRADED FUNCTION: compute_gradient
-def compute_gradient(X, y, w, b, lambda_=None): 
+#week2: linear regression
+
+import numpy as np
+import matplotlib.pyplot as plt
+from utils import *
+import copy
+import math
+%matplotlib inline
+
+
+## 2.1 know about data set
+
+# load the dataset
+x_train, y_train = load_data()
+
+# print x_train
+print("Type of x_train:",type(x_train))
+print("First five elements of x_train are:\n", x_train[:5]) 
+
+# print y_train
+print("Type of y_train:",type(y_train))
+print("First five elements of y_train are:\n", y_train[:5])  
+
+# check the dimensions of x and y
+print ('The shape of x_train is:', x_train.shape)
+print ('The shape of y_train is: ', y_train.shape)
+print ('Number of training examples (m):', len(x_train))
+
+# visualize the data set
+plt.scatter(x_train, y_train, marker='x', c='r') 
+
+plt.title("Profits vs. Population per city") # Set the title
+
+plt.ylabel('Profit in $10,000')# Set the y-axis label
+
+plt.xlabel('Population of City in 10,000s')# Set the x-axis label
+
+plt.show()
+
+
+## 2.1 compute cost
+
+
+def compute_cost(x, y, w, b): 
     """
-    Computes the gradient for logistic regression 
- 
-    Args:
-      X : (ndarray Shape (m,n)) variable such as house size 
-      y : (array_like Shape (m,1)) actual value 
-      w : (array_like Shape (n,1)) values of parameters of the model      
-      b : (scalar)                 value of parameter of the model 
-      lambda_: unused placeholder.
-    Returns
-      dj_dw: (array_like Shape (n,1)) The gradient of the cost w.r.t. the parameters w. 
-      dj_db: (scalar)                The gradient of the cost w.r.t. the parameter b. 
-    """
-    m, n = X.shape
-    cost_w = np.zeros(w.shape)
-    cost_b = 0.
-
-    ### START CODE HERE ### 
-    for i in range(m):
-
-
-        z_w = 0        
-
-        for j in range(n):
-            z_w += X[i][j]*w[j]
-        z_w += b
-
+    Computes the cost function for linear regression.
     
-    #    print(sigmoid(z_w))
-        f_yi = sigmoid(z_w)-y[i]
+    Args:
+        x (ndarray): Shape (m,) Input to the model (Population of cities) 
+        y (ndarray): Shape (m,) Label (Actual profits for the cities)
+        w, b (scalar): Parameters of the model
+    
+    Returns
+        total_cost (float): The cost of using w,b as the parameters for linear regression
+               to fit the data points in x and y
+    """
+    # number of training examples
+    m = x.shape[0] 
+    
+    total_cost = 0
+    
+    cost=(x*w+b-y)**2
 
-
-        cost_b += f_yi
-
-    #    print(cost_b)
-
-        for j in range(n):
-            f_yxi= f_yi*X[i][j]
-            cost_w[j] += f_yxi
-
-
-    dj_db = cost_b /m
-    dj_dw = cost_w*(1/m)
-    ### END CODE HERE ###
-
+    cost_j=0
+    for i in cost:
+        cost_j=cost_j+i
         
-    return dj_db, dj_dw
+## 2.2 gradient descent
+
+def compute_gradient(x, y, w, b): 
+    """
+    Computes the gradient for linear regression 
+    Args:
+      x (ndarray): Shape (m,) Input to the model (Population of cities) 
+      y (ndarray): Shape (m,) Label (Actual profits for the cities)
+      w, b (scalar): Parameters of the model  
+    Returns
+      dj_dw (scalar): The gradient of the cost w.r.t. the parameters w
+      dj_db (scalar): The gradient of the cost w.r.t. the parameter b     
+     """
+    
+    # Number of training examples
+    m = x.shape[0]
+    
+    # You need to return the following variables correctly
+    dj_dw = 0
+    dj_db = 0
+        
+    grd_db=w*x+b-y
+    grd_db_sum=0
+
+    for i in grd_db:
+        grd_db_sum=grd_db_sum+i
+
+    dj_db=grd_db_sum/m       
+
+    grd_dw=(w*x+b-y)*x
+
+    grd_dw_sum=0
+    for i in grd_dw:
+        grd_dw_sum=grd_dw_sum+i
+
+    dj_dw=grd_dw_sum/m       
+        
+    return dj_dw, dj_db
+
+
+    total_cost=cost_j/(2*m)
+    total_cost
+
+    return total_cost
+
+
+## 2.3 learning parameters 
+def gradient_descent(x, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters): 
+    """
+    Performs batch gradient descent to learn theta. Updates theta by taking 
+    num_iters gradient steps with learning rate alpha
+    
+    Args:
+      x :    (ndarray): Shape (m,)
+      y :    (ndarray): Shape (m,)
+      w_in, b_in : (scalar) Initial values of parameters of the model
+      cost_function: function to compute cost
+      gradient_function: function to compute the gradient
+      alpha : (float) Learning rate
+      num_iters : (int) number of iterations to run gradient descent
+    Returns
+      w : (ndarray): Shape (1,) Updated values of parameters of the model after
+          running gradient descent
+      b : (scalar)                Updated value of parameter of the model after
+          running gradient descent
+    """
+    
+    # number of training examples
+    m = len(x)
+    
+    # An array to store cost J and w's at each iteration — primarily for graphing later
+    J_history = []
+    w_history = []
+    w = copy.deepcopy(w_in)  #avoid modifying global w within function
+    b = b_in
+    
+    for i in range(num_iters):
+
+        # Calculate the gradient and update the parameters
+        dj_dw, dj_db = gradient_function(x, y, w, b )  
+
+        # Update Parameters using w, b, alpha and gradient
+        w = w - alpha * dj_dw               
+        b = b - alpha * dj_db               
+
+        # Save cost J at each iteration
+        if i<100000:      # prevent resource exhaustion 
+            cost =  cost_function(x, y, w, b)
+            J_history.append(cost)
+
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters/10) == 0:
+            w_history.append(w)
+            print(f"Iteration {i:4}: Cost {float(J_history[-1]):8.2f}   ")
+        
+    return w, b, J_history, w_history #return w and J,w history for graphing
